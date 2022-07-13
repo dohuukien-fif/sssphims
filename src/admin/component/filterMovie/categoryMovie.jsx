bd8abb9c563877051833a56f9fd5b9eb2d636cdc
huu kien <huukien0099@gmail.com>
2022-07-13 13:04:54 +0700

    update admin

---
+++ src/admin/component/filterMovie/categoryMovie.jsx
@@ -0,0 +1,74 @@
+import React, { Fragment, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import ProductApi from "../../../api/movieHome";
+import "./category.scss";
+const CategoryMovie = ({ handleSubmitValueUp, ApiFilter }) => {
+  const [Nation, setNation] = useState([]);
+  const [Category, setCategory] = useState([]);
+  const [year, setyear] = useState([]);
+  const [value, setValue] = useState([]);
+  useEffect(() => {
+    const fetchApi = async () => {
+      const res = await ApiFilter.getAll();
+
+      setNation([...new Set(res.map((item) => item.nation))]);
+      setCategory([...new Set(res.map((item) => item.category))]);
+      setyear([...new Set(res.map((item) => item.year))]);
+    };
+    fetchApi();
+  }, []);
+
+  const handleSelectChange = (e) => {
+    const { value, name } = e.target;
+
+    setValue((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleClickSubmitBroveValue = () => {
+    if (handleSubmitValueUp) handleSubmitValueUp(value);
+  };
+  return (
+    <div className="filter__category">
+      <div className="filter__group">
+        <select name="category" onChange={handleSelectChange}>
+          <option value="">Vui lòng chọn phim</option>
+          {Category.map((item, idx) => (
+            <Fragment>
+              <option value={item}>{item}</option>
+            </Fragment>
+          ))}
+        </select>
+      </div>
+      <div className="filter__group">
+        <select name="nation" onChange={handleSelectChange}>
+          <option value="">Chọn quốc gia</option>
+          {Nation.map((item, idx) => (
+            <Fragment>
+              <option value={item}>{item}</option>
+            </Fragment>
+          ))}
+        </select>
+      </div>
+      <div className="filter__group">
+        <select name="year" onChange={handleSelectChange}>
+          <option value="">Vui lòng chọn phim</option>
+          {year.map((item, idx) => (
+            <Fragment>
+              <option value={item}>{item}</option>
+            </Fragment>
+          ))}
+        </select>
+      </div>
+      <div className="filter__category--btn">
+        <button onClick={handleClickSubmitBroveValue}>Lọc</button>
+      </div>
+    </div>
+  );
+};
+
+CategoryMovie.propTypes = {};
+
+export default CategoryMovie;
